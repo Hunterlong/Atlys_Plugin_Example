81cb485188618f6980d90e45392bb7ec8acca5ae
hunterlong <info@socialeck.com>
2016-11-15 16:14:01 -0700

    new

--- lib/examplegem/engine.rb
+++ lib/examplegem/engine.rb
@@ -1,5 +1,12 @@
 module Examplegem
   class Engine < ::Rails::Engine
     isolate_namespace Examplegem
+
+    config.to_prepare do
+      # Make the implementing application's helpers available to the engine.
+      # This is required for the overriding of engine views and helpers to work correctly.
+      Examplegem::ApplicationController.helper Rails.application.helpers
+    end
+
   end
 end
