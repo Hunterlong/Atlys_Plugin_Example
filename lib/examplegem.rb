9cbab8000228f36725c74f7a6726d66a5dbee0cc
hunterlong <info@socialeck.com>
2016-11-15 15:42:32 -0700

    new

--- lib/examplegem.rb
+++ lib/examplegem.rb
@@ -1,3 +1,13 @@
 module Examplegem
+  require "active_support/dependencies"
+
+  mattr_accessor :app_root
+
+  # Yield self on setup for nice config blocks
+  def self.setup
+    yield self
+  end
+
   require 'examplegem/engine'
+
 end
