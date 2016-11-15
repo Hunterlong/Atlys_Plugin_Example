9b8c9a82de920ee926d6863d068edd05ea96520e
hunterlong <info@socialeck.com>
2016-11-15 15:26:49 -0700

    new

--- lib/examplegem/engine.rb
+++ lib/examplegem/engine.rb
@@ -1,16 +1,5 @@
 module Examplegem
   class Engine < ::Rails::Engine
     isolate_namespace Examplegem
-
-	    initialize "Examplegem.load_app_instance_data" do |app|
-	      Examplegem.setup do |config|
-		config.app_root = app.root
-	      end
-	    end
-
-	    initialize "Examplegem.load_static_assets" do |app|
-	      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-	    end
-
   end
 end
