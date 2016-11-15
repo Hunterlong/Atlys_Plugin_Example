e28aab0f1b38c7f75abceb2a4ec07db690d467bc
hunterlong <info@socialeck.com>
2016-11-15 13:23:06 -0700

    new

---
+++ app/controllers/example_controller.rb
@@ -0,0 +1,9 @@
+module Examplegem
+  class ExampleController < ApplicationController
+
+	def index
+
+	end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  isolate_namespace Examplegem
   mount Examplegem::Engine => "/examplegem"
+  root to: "example#index"
 end
